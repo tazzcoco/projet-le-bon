6b023124a96b61b0e47cf094c12873670a84ac83
MrTyre <tommy.riviere@free.fr>
2014-05-27 15:44:11 +0200

    Fin des modifications biiiitch !

--- src/princetonPlainsboro/Date.java
+++ src/princetonPlainsboro/Date.java
@@ -4,12 +4,24 @@ class Date implements Comparable {
     private int jour;
     private int mois;
     private int annee;
+    private int heure;
+    private int minute;
+
     
     public Date(int jour, int mois, int annee) {
         this.jour = jour;
         this.mois = mois;
         this.annee = annee;
         }
+    
+    public Date(int jour, int mois, int annee, int heure, int minute) {
+        this.jour = jour;
+        this.mois = mois;
+        this.annee = annee;
+        this.heure = heure;
+        this.minute = minute;
+    }
+
     
     public String toString() {
         return jour + "/" + mois + "/" + annee;
--- src/princetonPlainsboro/Patient.java
+++ src/princetonPlainsboro/Patient.java
@@ -1,25 +1,59 @@
 package princetonPlainsboro;
 
 class Patient {
+
     private String nom;
     private String prenom;
-    
+    private int numSecu;
+    private String adresse;
+    private Date dateNaissance;
+
     public Patient(String nom, String prenom) {
         this.nom = nom;
         this.prenom = prenom;
-        }
-    
-    public String toString() {
-        return prenom + " " + nom;
-        }
-    
-    public boolean equals(Object o) {
-        if (o instanceof Patient) {
-            Patient p = (Patient)o;
-            return nom.equals(p.nom) && prenom.equals(p.prenom);
-            }
-        else
-            return false;
-        }    
     }
 
+    public Patient(String nom, String prenom, int numSecu, String adresse) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.numSecu = numSecu;
+        this.adresse = adresse;
+    }
+
+    public String toString() {
+        return prenom + " " + nom;
+    }
+
+    public boolean equals(Object o) {
+        if (o instanceof Patient) {
+            Patient p = (Patient) o;
+            return nom.equals(p.nom) && prenom.equals(p.prenom);
+        } else {
+            return false;
+        }
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public int getNumSecu() {
+        return numSecu;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public void afficherDP() {
+
+    }
+}
